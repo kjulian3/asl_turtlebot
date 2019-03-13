8b0d4f6ebb76efc6ec5477c6ad9f7b876002dc65
Kyle Julian <kjulian3@stanford.edu>
2019-03-13 16:58:14 -0700

    Markers combined into an array

--- scripts/pub_marker.py
+++ scripts/pub_marker.py
@@ -13,10 +13,8 @@ mapping = rospy.get_param("map")
 class Turtlebot_Marker:
 	def __init__(self):
 		rospy.init_node('pub_marker')
-		self.circle_pub = rospy.Publisher("turtlebot_circle", Marker, queue_size=10)
-		self.goal_circle_pub = rospy.Publisher("turtlebot_goal_circle", Marker, queue_size=10)
-		self.arrow_pub = rospy.Publisher("turtlebot_arrow", Marker, queue_size=10)
-		self.goal_arrow_pub = rospy.Publisher("turtlebot_goal_arrow", Marker, queue_size=10)
+		self.turtlebot_pub = rospy.Publisher("markers_turtlebot", MarkerArray, queue_size=10)
+		self.goal_pub = rospy.Publisher("markers_goal", MarkerArray, queue_size=10)
 		self.goal_pose  = None
 		self.goal_pose_arrow = None
 		self.trans_listener = tf.TransformListener()
@@ -79,80 +77,83 @@ class Turtlebot_Marker:
 		self.food_pub.publish(markerArray)
 
 
-	def make_marker(self):
-		self.slam_marker = Marker()
-		self.slam_marker.header.frame_id = "/base_footprint"
-		self.slam_marker.header.stamp = rospy.Time(0)
-		self.slam_marker.ns = "turtlebot_circle"
-		self.slam_marker.action=Marker.ADD
-		self.slam_marker.type = 3    
-		self.slam_marker.scale.x = .15
-		self.slam_marker.scale.y = .15
-		self.slam_marker.scale.z = .01
-		self.slam_marker.color.r = 0.0
-		self.slam_marker.color.g = 1.0
-		self.slam_marker.color.b = 1.0
-		self.slam_marker.color.a = 1.0
-		self.slam_marker.lifetime = rospy.Duration(20)
-
-		self.arrow_marker = Marker()
-		self.arrow_marker.header.frame_id = "/base_footprint"
-		self.arrow_marker.header.stamp = rospy.Time(0)
-		self.arrow_marker.ns = "turtlebot_arrow"
-		self.arrow_marker.action=Marker.ADD
-		self.arrow_marker.type = 0    # line list
-		self.arrow_marker.pose = Pose(Point(0.075, 0.0, 0.01), Quaternion(0, 0, 0, 1))
-		self.arrow_marker.scale.x = .25
-		self.arrow_marker.scale.y = .05
-		self.arrow_marker.scale.z = .05
-		self.arrow_marker.color.r = 0.0
-		self.arrow_marker.color.g = 0.0
-		self.arrow_marker.color.b = 1.0
-		self.arrow_marker.color.a = 1.0
-		self.arrow_marker.lifetime = rospy.Duration(20)
+	def pose_markers(self):
 
 		if self.goal_pose is not None:
+			goal_marker_array = MarkerArray()
 			origin_frame = "/map" if mapping else "/odom"
-			self.goal_marker = Marker()
-			self.goal_marker.header.frame_id = origin_frame
-			self.goal_marker.header.stamp = rospy.Time(0)
-			self.goal_marker.ns = "goal_circle"
-			self.goal_marker.action = Marker.ADD
-			self.goal_marker.pose = self.goal_pose
-			self.goal_marker.type=3  
-			self.goal_marker.scale.x = .15
-			self.goal_marker.scale.y = .15
-			self.goal_marker.scale.z = .01
-			self.goal_marker.color.r = 0.0
-			self.goal_marker.color.g = 1.0
-			self.goal_marker.color.b = 0.0
-			self.goal_marker.color.a = 0.5
-			self.goal_marker.lifetime = rospy.Duration(20)
+			goal_marker = Marker()
+			goal_marker.header.frame_id = origin_frame
+			goal_marker.header.stamp = rospy.Time(0)
+			goal_marker.ns = "goal_circle"
+			goal_marker.action = Marker.ADD
+			goal_marker.pose = self.goal_pose
+			goal_marker.type=3  
+			goal_marker.scale.x = .15
+			goal_marker.scale.y = .15
+			goal_marker.scale.z = .01
+			goal_marker.color.r = 0.0
+			goal_marker.color.g = 1.0
+			goal_marker.color.b = 0.0
+			goal_marker.color.a = 0.5
+			goal_marker.lifetime = rospy.Duration(600)
+			goal_marker_array.markers.append(goal_marker)
 
-			self.goal_marker_arrow = Marker()
-			self.goal_marker_arrow.header.frame_id = origin_frame
-			self.goal_marker_arrow.header.stamp = rospy.Time(0)
-			self.goal_marker_arrow.ns = "goal_arrow"
-			self.goal_marker_arrow.action = Marker.ADD
-			self.goal_marker_arrow.pose = self.goal_pose_arrow
-			self.goal_marker_arrow.type=0 
-			self.goal_marker_arrow.scale.x = .25
-			self.goal_marker_arrow.scale.y = .05
-			self.goal_marker_arrow.scale.z = .05
-			self.goal_marker_arrow.color.r = 0.0
-			self.goal_marker_arrow.color.g = 1.0
-			self.goal_marker_arrow.color.b = 0.0
-			self.goal_marker_arrow.color.a = 0.5
-			self.goal_marker_arrow.lifetime = rospy.Duration(20)
+			goal_marker_arrow = Marker()
+			goal_marker_arrow.header.frame_id = origin_frame
+			goal_marker_arrow.header.stamp = rospy.Time(0)
+			goal_marker_arrow.ns = "goal_arrow"
+			goal_marker_arrow.action = Marker.ADD
+			goal_marker_arrow.pose = self.goal_pose_arrow
+			goal_marker_arrow.type=0 
+			goal_marker_arrow.scale.x = .25
+			goal_marker_arrow.scale.y = .05
+			goal_marker_arrow.scale.z = .05
+			goal_marker_arrow.color.r = 0.0
+			goal_marker_arrow.color.g = 1.0
+			goal_marker_arrow.color.b = 0.0
+			goal_marker_arrow.color.a = 0.5
+			goal_marker_arrow.lifetime = rospy.Duration(600)
+			goal_marker_array.markers.append(goal_marker_arrow)
 
+			self.goal_pub.publish(goal_marker_array)
 
-			self.goal_circle_pub.publish(self.goal_marker)
-			self.goal_arrow_pub.publish(self.goal_marker_arrow)
+		markerArray = MarkerArray()
+		slam_marker = Marker()
+		slam_marker.header.frame_id = "/base_footprint"
+		slam_marker.header.stamp = rospy.Time(0)
+		slam_marker.ns = "turtlebot_circle"
+		slam_marker.action=Marker.ADD
+		slam_marker.type = 3    
+		slam_marker.scale.x = .15
+		slam_marker.scale.y = .15
+		slam_marker.scale.z = .01
+		slam_marker.color.r = 0.0
+		slam_marker.color.g = 1.0
+		slam_marker.color.b = 1.0
+		slam_marker.color.a = 1.0
+		slam_marker.lifetime = rospy.Duration(20)
+		markerArray.markers.append(slam_marker)
 
+		arrow_marker = Marker()
+		arrow_marker.header.frame_id = "/base_footprint"
+		arrow_marker.header.stamp = rospy.Time(0)
+		arrow_marker.ns = "turtlebot_arrow"
+		arrow_marker.action=Marker.ADD
+		arrow_marker.type = 0    # line list
+		arrow_marker.pose = Pose(Point(0.075, 0.0, 0.01), Quaternion(0, 0, 0, 1))
+		arrow_marker.scale.x = .25
+		arrow_marker.scale.y = .05
+		arrow_marker.scale.z = .05
+		arrow_marker.color.r = 0.0
+		arrow_marker.color.g = 0.0
+		arrow_marker.color.b = 1.0
+		arrow_marker.color.a = 1.0
+		arrow_marker.lifetime = rospy.Duration(20)
+		markerArray.markers.append(arrow_marker)
 
+		self.turtlebot_pub.publish(markerArray)
 
-		self.circle_pub.publish(self.slam_marker)
-		self.arrow_pub.publish(self.arrow_marker)
 
 	def goal_callback(self, msg):
 
@@ -205,7 +206,7 @@ class Turtlebot_Marker:
 	def run(self):
 		while not rospy.is_shutdown():
 			rospy.sleep(0.01)
-			self.make_marker()
+			self.pose_markers()
 
 
 if __name__ == '__main__':
